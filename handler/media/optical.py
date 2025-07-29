b652372957b3bfe415e22de2dc9af0695bc5bc4c
Shelby Jueden <shelby@jueden.org>
2025-07-29 12:49:12 -0700

    Add optical handler

---
+++ handler/media/optical.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# CD ripping module for pyDiscRip. Can be used to rip a CD and fetch metadata
+
+# Python System
+import os
+import json
+from pathlib import Path
+import time
+from pprint import pprint
+
+# External Modules
+import libdiscid
+import musicbrainzngs
+import pycdio, cdio
+
+# Internal Modules
+from handler.media.media_handler import MediaHandler
+
+
+class MediaOptical(MediaHandler):
+    """Handler for CD media types
+
+    rips using a subprocess command to run `cdrdao` to create a BIN/CUE
+    """
+
+    def __init__(self):
+        """Constructor to setup basic data and config defaults
+
+        """
+        # Call parent constructor
+        super().__init__()
+        # Set media type to handle
+        self.type_id="OPTICAL"
+        # Default config data
+        self.config_data={}
+        # Data types output
+        self.data_outputs=[]
+        self.cd_tracks=0
+
+
+    def load(self,media_sample,bypass=False):
+        """Load media before continuing.
+
+        Default method call waits for user to press enter
+
+        Overload with automatic methods where possible.
+        """
+        print("Waiting for disc access")
+        wait_load=0
+        while(True):
+            try:
+                d=cdio.Device(media_sample["drive"])
+                tracks = d.get_num_tracks()
+                print(f"Found disc with {drive_name} tracks")
+                time.sleep(wait_load)
+                return
+            except cdio.TrackError:
+                print("Please insert disc")
+                time.sleep(3)
+                wait_load=10
+
+    def eject(self,media_sample):
+        """Eject drive tray
+        """
+        d=cdio.Device(media_sample["drive"])
+        d.eject_media()
+        time.sleep(2)
