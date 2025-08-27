8932f1f1d8b5b7c75d22592ebcecbeb31ade9239
Shelby Jueden <shelby@jueden.org>
2025-08-27 08:52:12 -0700

    catch musibrainz error

--- handler/media/cd.py
+++ handler/media/cd.py
@@ -222,11 +222,14 @@ class MediaHandlerCD(MediaOptical):
         # Determine number of seesions to rip
         self.countSessions(media_sample)
         # Get metadata for audio CD
-        data_output = self.fetchMetadata(media_sample)
+        try:
+            data_output = self.fetchMetadata(media_sample)
 
-        # Add metadata if was found
-        if data_output is not None:
-                datas.append(data_output)
+            # Add metadata if was found
+            if data_output is not None:
+                    datas.append(data_output)
+        except Exception as e:
+            print("Unable to fetch musicbrainz data")
 
         # cd-info log
         result = self.osRun(["cd-info", f"{media_sample["drive"]}"])
